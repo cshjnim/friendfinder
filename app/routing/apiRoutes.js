f6abe66a4c824d937ebd627a5022822b111a4d4e
Hyejin Kim <cshjkim@gmail.com>
2020-01-13 20:37:25 -0800

    updating and edit apiroutes js

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,3 +1,50 @@
 // * A GET route with the url `/api/friends`. This will be used to display a JSON of all possible friends.
 // * A POST routes `/api/friends`. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
 
+
+var friends = require("../data/friends");
+
+module.exports = function(app) {
+  // Return all friends found in friends.js as JSON
+  app.get("/api/friends", function(req, res) {
+    res.json(friends);
+  });
+
+  app.post("/api/friends", function(req, res) {
+    console.log(req.body.scores);
+
+    // Receive user details (name, photo, scores)
+    var user = req.body;
+
+    // parseInt for scores
+    for(var i = 0; i < user.scores.length; i++) {
+      user.scores[i] = parseInt(user.scores[i]);
+    }
+
+    // default friend match is the first friend but result will be whoever has the minimum difference in scores
+    var friendIndex = 0;
+    var minimumDifference = 40;
+
+    // in this for-loop, start off with a zero difference and compare the user and the ith friend scores, one set at a time
+    //  whatever the difference is, add to the total difference
+    for(var i = 0; i < friends.length; i++) {
+      var totalDifference = 0;
+      for(var j = 0; j < friends[i].scores.length; j++) {
+        var difference = Math.abs(user.scores[j] - friends[i].scores[j]);
+        totalDifference += difference;
+      }
+
+      // if there is a new minimum, change the best friend index and set the new minimum for next iteration comparisons
+      if(totalDifference < minimumDifference) {
+        friendIndex = i;
+        minimumDifference = totalDifference;
+      }
+    }
+
+    // after finding match, add user to friend array
+    friends.push(user);
+
+    // send back to browser the best friend match
+    res.json(friends[friendIndex]);
+  });
+};
