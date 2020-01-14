f88830d61f3ef848355f1afa69618464de87db32
Hyejin Kim <cshjkim@gmail.com>
2020-01-13 21:11:14 -0800

    updating friends.js to add more friends and survey.html to update the modal

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,10 +1,30 @@
 //an array of objects. Each of these objects should roughly follow the format below.
 
-var friends = [{
+var friends = [
+  {
   name: "Ahmed",
   photo:
-    "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "https://upload.wikimedia.org/wikipedia/commons/d/dc/Riz_Ahmed_at_the_Rogue_One_-_A_Star_Wars_Story_-_World_Premeire_Red_Carpet_-_DSC_0437_%2831547570706%29_%28cropped%29.jpg",
   scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
-}];
+},
+{
+  name: "Chloe",
+  photo:
+    "https://partycity6.scene7.com/is/image/PartyCity/_pdp_sq_?$_1000x1000_$&$product=PartyCity/294138",
+  scores: [2, 3, 1, 2, 3, 2, 1, 5, 4, 2]
+},
+{
+  name: "David",
+  photo:
+    "http://www.accademia.org/wp-content/uploads/2014/01/david-head-760x506.jpg",
+  scores: [1, 2 , 1, 2, 1, 1, 1, 1, 1, 1]
+},
+{
+  name: "Superman",
+  photo:
+    "https://filmgoblin.com/wp-content/uploads/2019/12/HenryCavillSuperman.jpeg",
+  scores: [4, 3, 2, 1 , 4, 3, 2, 1, 4, 2]
+}
+];
 
 module.exports = friends;
